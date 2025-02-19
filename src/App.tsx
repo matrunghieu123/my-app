e25f21928d2509ad446d945c992714264c3b9a52
matrunghieu123 <hieumatrungd123@gmail.com>
2025-02-19 17:00:49 +0700

    3commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
-  const [isChatVisible, setIsChatVisible] = useState(false);
+  const [isChatVisible, setIsChatVisible] = useState(true);
   const [phoneError, setPhoneError] = useState('');
   const [emailError, setEmailError] = useState('');
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
@@ -27,6 +27,11 @@ function App() {
   };
 
   const handleSendMessage = () => {
+    if (!name || !phone) {
+      setIsChatVisible(false);
+      return;
+    }
+
     if (inputValue.trim() || selectedFiles.length > 0) {
       const newMessage = {
         text: inputValue,
@@ -42,6 +47,12 @@ function App() {
       setMessages([...messages, newMessage]);
       setInputValue('');
       setSelectedFiles([]);
+    }
+  };
+
+  const handleInputFocus = () => {
+    if (!name || !phone) {
+      setIsChatVisible(false);
     }
   };
 
@@ -86,6 +97,10 @@ function App() {
     }
   };
 
+  const handleCancelRegistration = () => {
+    setIsChatVisible(true);
+  };
+
   const shouldDisplayTimestamp = (index: number) => {
     if (index === 0) return true;
     const currentMessage = messages[index];
@@ -113,156 +128,167 @@ function App() {
             <button className="close-button" onClick={closeModal}>
               <FontAwesomeIcon icon={faTimes} />
             </button>
-            {!isChatVisible ? (
-              <div className="user-info-form">
-                <div style={{ marginBottom: '10px' }}>
-                  <label style={{ display: 'flex', marginBottom: '5px' }}>Tên</label>
-                  <Input
-                    placeholder="Nhập tên"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    style={{ width: '100%' }}
-                  />
+            <div className="chat-container">
+              <div style={{ marginBottom: '20px', textAlign: 'center', color: '#888' }}>
+                Cảm ơn bạn đã cung cấp thông tin
+                <div style={{ color: '#00f', marginTop: '5px' }}>
+                  {name} | {phone} | {email}
                 </div>
-                <div style={{ marginBottom: '10px' }}>
-                  <label style={{ display: 'flex', marginBottom: '5px' }}>
-                    Số điện thoại <span style={{ color: 'red' }}>*</span>
-                  </label>
-                  <Input
-                    placeholder="Nhập số điện thoại (Bắt buộc)"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    style={{ width: '100%' }}
-                  />
-                  {phoneError && <div style={{ color: 'red', marginTop: '5px' }}>{phoneError}</div>}
-                </div>
-                <div style={{ marginBottom: '10px' }}>
-                  <label style={{ display: 'flex', marginBottom: '5px' }}>Email</label>
-                  <Input
-                    placeholder="Nhập email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    style={{ width: '100%' }}
-                  />
-                  {emailError && <div style={{ color: 'red', marginTop: '5px' }}>{emailError}</div>}
-                </div>
-                <Button type="primary" onClick={handleOpenChat}>
-                  Xác nhận
-                </Button>
               </div>
-            ) : (
-              <div className="chat-container">
-                <div style={{ marginBottom: '20px', textAlign: 'center', color: '#888' }}>
-                  Cảm ơn bạn đã cung cấp thông tin
-                  <div style={{ color: '#00f', marginTop: '5px' }}>
-                    {name} | {phone} | {email}
-                  </div>
-                </div>
-                <div className="messages">
-                  {messages.map((msg, index) => (
-                    <div key={index} style={{ marginBottom: '10px', textAlign: 'right' }}>
-                      {shouldDisplayTimestamp(index) && (
-                        <div style={{ color: '#888', fontSize: '12px' }}>Tôi, {msg.time}</div>
-                      )}
-                      <div style={{
-                        display: 'inline-block',
-                        backgroundColor: '#1e3a8a',
-                        color: '#fff',
-                        padding: '8px 12px',
-                        borderRadius: '15px',
-                        maxWidth: '60%',
-                        wordWrap: 'break-word',
-                      }}>
-                        {msg.text}
-                        {msg.images && msg.images.map((image, idx) => (
-                          <div key={idx} style={{ marginTop: '10px' }}>
-                            <img src={image} alt={`uploaded-${idx}`} style={{ maxWidth: '100%', borderRadius: '10px' }} />
-                          </div>
-                        ))}
-                        {msg.videos && msg.videos.map((video, idx) => (
-                          <div key={idx} style={{ marginTop: '10px' }}>
-                            <video controls style={{ maxWidth: '100%', borderRadius: '10px' }}>
-                              <source src={video} type="video/mp4" />
-                              Your browser does not support the video tag.
-                            </video>
-                          </div>
-                        ))}
-                        {msg.files && msg.files.map((file, idx) => (
-                          <div key={idx} style={{ marginTop: '10px' }}>
-                            <a href={file.url} download={file.name} style={{ color: '#fff', textDecoration: 'none' }}>
-                              <FontAwesomeIcon icon={file.type.startsWith('image/') ? faFileImage : file.type.startsWith('video/') ? faFileVideo : faFile} />
-                              <span style={{ marginLeft: '5px' }}>{file.name}</span>
-                            </a>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="file-preview-container" style={{ marginBottom: '10px' }}>
-                  {selectedFiles.map((file, index) => (
-                    <div key={index} style={{
+              <div className="messages">
+                {messages.map((msg, index) => (
+                  <div key={index} style={{ marginBottom: '10px', textAlign: 'right' }}>
+                    {shouldDisplayTimestamp(index) && (
+                      <div style={{ color: '#888', fontSize: '12px' }}>Tôi, {msg.time}</div>
+                    )}
+                    <div style={{
                       display: 'inline-block',
-                      marginRight: '10px',
-                      position: 'relative',
-                      border: '1px solid #ccc',
-                      borderRadius: '5px',
-                      padding: '5px',
-                      backgroundColor: '#f0f0f0'
+                      backgroundColor: '#1e3a8a',
+                      color: '#fff',
+                      padding: '8px 12px',
+                      borderRadius: '15px',
+                      maxWidth: '60%',
+                      wordWrap: 'break-word',
                     }}>
-                      {file.type.startsWith('image/') ? (
-                        <img
-                          src={URL.createObjectURL(file)}
-                          alt={file.name}
-                          style={{ width: '50px', height: '50px', objectFit: 'cover', borderRadius: '5px' }}
-                        />
-                      ) : file.type.startsWith('video/') ? (
-                        <video controls style={{ width: '50px', height: '50px', objectFit: 'cover', borderRadius: '5px' }}>
-                          <source src={URL.createObjectURL(file)} type="video/mp4" />
-                          Your browser does not support the video tag.
-                        </video>
-                      ) : (
-                        <>
-                          <FontAwesomeIcon icon={faFile} />
-                          <span style={{ marginLeft: '5px' }}>{file.name}</span>
-                        </>
-                      )}
-                      <FontAwesomeIcon
-                        icon={faTimes}
-                        style={{
-                          position: 'absolute',
-                          top: '-5px',
-                          right: '-5px',
-                          cursor: 'pointer',
-                          color: 'red'
-                        }}
-                        onClick={() => handleRemoveFile(file)}
+                      {msg.text}
+                      {msg.images && msg.images.map((image, idx) => (
+                        <div key={idx} style={{ marginTop: '10px' }}>
+                          <img src={image} alt={`uploaded-${idx}`} style={{ maxWidth: '100%', borderRadius: '10px' }} />
+                        </div>
+                      ))}
+                      {msg.videos && msg.videos.map((video, idx) => (
+                        <div key={idx} style={{ marginTop: '10px' }}>
+                          <video controls style={{ maxWidth: '100%', borderRadius: '10px' }}>
+                            <source src={video} type="video/mp4" />
+                            Your browser does not support the video tag.
+                          </video>
+                        </div>
+                      ))}
+                      {msg.files && msg.files.map((file, idx) => (
+                        <div key={idx} style={{ marginTop: '10px' }}>
+                          <a href={file.url} download={file.name} style={{ color: '#fff', textDecoration: 'none' }}>
+                            <FontAwesomeIcon icon={file.type.startsWith('image/') ? faFileImage : file.type.startsWith('video/') ? faFileVideo : faFile} />
+                            <span style={{ marginLeft: '5px' }}>{file.name}</span>
+                          </a>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="file-preview-container" style={{ marginBottom: '10px' }}>
+                {selectedFiles.map((file, index) => (
+                  <div key={index} style={{
+                    display: 'inline-block',
+                    marginRight: '10px',
+                    position: 'relative',
+                    border: '1px solid #ccc',
+                    borderRadius: '5px',
+                    padding: '5px',
+                    backgroundColor: '#f0f0f0'
+                  }}>
+                    {file.type.startsWith('image/') ? (
+                      <img
+                        src={URL.createObjectURL(file)}
+                        alt={file.name}
+                        style={{ width: '50px', height: '50px', objectFit: 'cover', borderRadius: '5px' }}
                       />
-                    </div>
-                  ))}
-                </div>
-                <div className="chat-input">
-                  <Input
-                    placeholder="Nhập tin nhắn"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                    onPressEnter={handleSendMessage}
-                    style={{ fontSize: '16px', height: '40px' }}
-                    suffix={
+                    ) : file.type.startsWith('video/') ? (
+                      <video controls style={{ width: '50px', height: '50px', objectFit: 'cover', borderRadius: '5px' }}>
+                        <source src={URL.createObjectURL(file)} type="video/mp4" />
+                        Your browser does not support the video tag.
+                      </video>
+                    ) : (
                       <>
-                        <FontAwesomeIcon icon={faSmile} style={{ marginRight: 12 }} onClick={() => setShowEmojiPicker(!showEmojiPicker)} />
-                        <Upload beforeUpload={handleFileUpload} showUploadList={false}>
-                          <FontAwesomeIcon icon={faPaperclip} style={{ marginRight: 5 }} />
-                        </Upload>
-                        <FontAwesomeIcon icon={faPaperPlane} onClick={handleSendMessage} />
+                        <FontAwesomeIcon icon={faFile} />
+                        <span style={{ marginLeft: '5px' }}>{file.name}</span>
                       </>
-                    }
-                  />
-                  {showEmojiPicker && (
-                    <div className="emoji-picker-container">
-                      <EmojiPicker onEmojiClick={handleEmojiClick} />
-                    </div>
-                  )}
+                    )}
+                    <FontAwesomeIcon
+                      icon={faTimes}
+                      style={{
+                        position: 'absolute',
+                        top: '-5px',
+                        right: '-5px',
+                        cursor: 'pointer',
+                        color: 'red'
+                      }}
+                      onClick={() => handleRemoveFile(file)}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="chat-input">
+                <Input
+                  placeholder="Nhập tin nhắn"
+                  value={inputValue}
+                  onFocus={handleInputFocus}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  onPressEnter={handleSendMessage}
+                  style={{ fontSize: '16px', height: '40px' }}
+                  suffix={
+                    <>
+                      <FontAwesomeIcon icon={faSmile} style={{ marginRight: 12 }} onClick={() => setShowEmojiPicker(!showEmojiPicker)} />
+                      <Upload beforeUpload={handleFileUpload} showUploadList={false}>
+                        <FontAwesomeIcon icon={faPaperclip} style={{ marginRight: 5 }} />
+                      </Upload>
+                      <FontAwesomeIcon icon={faPaperPlane} onClick={handleSendMessage} />
+                    </>
+                  }
+                  disabled={!isChatVisible}
+                />
+                {showEmojiPicker && (
+                  <div className="emoji-picker-container">
+                    <EmojiPicker onEmojiClick={handleEmojiClick} />
+                  </div>
+                )}
+              </div>
+            </div>
+            {!isChatVisible && (
+              <div className="overlay">
+                <div className="user-info-form" style={{ marginTop: '170px' }}>
+                  <div className="header">
+                    <p>Vui lòng cho chúng tôi biết thêm một số thông tin của Quý Khách</p>
+                  </div>
+                  <div style={{ marginBottom: '20px' }}>
+                    <label style={{ display: 'flex', marginBottom: '5px' }}>Tên đầy đủ</label>
+                    <Input
+                      placeholder="Nhập tên đầy đủ"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      style={{ width: '100%' }}
+                    />
+                  </div>
+                  <div style={{ marginBottom: '20px' }}>
+                    <label style={{ display: 'flex', marginBottom: '10px' }}>
+                      Số điện thoại <span style={{ color: 'red' }}>*</span>
+                    </label>
+                    <Input
+                      placeholder="Nhập số điện thoại"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      style={{ width: '100%' }}
+                    />
+                    {phoneError && <div style={{ color: 'red', marginTop: '5px' }}>{phoneError}</div>}
+                  </div>
+                  <div style={{ marginBottom: '20px' }}>
+                    <label style={{ display: 'flex', marginBottom: '5px' }}>Email</label>
+                    <Input
+                      placeholder="Nhập email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      style={{ width: '100%' }}
+                    />
+                    {emailError && <div style={{ color: 'red', marginTop: '5px' }}>{emailError}</div>}
+                  </div>
+                  <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '30px' }}>
+                    <Button type="primary" onClick={handleOpenChat}>
+                      Gửi thông tin
+                    </Button>
+                    <Button onClick={handleCancelRegistration}>
+                      Để sau
+                    </Button>
+                  </div>
                 </div>
               </div>
             )}
